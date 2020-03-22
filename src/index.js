adc4f0967257f58cb1c2f9f5e16f367efbca402e
Serhii Chernysh <srg.chernysh@gmail.com>
2020-03-22 16:27:39 +0200

    feat: finish task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 module.exports = function toReadable (number) {
-  
+    if (number < 100) {
+        return toReadableLessOneHundred(number);
+    } else if (number % 100 === 0) {
+        return `${readableNumbersFromZeroToNineteen(String(number).slice(0, 1))} hundred`;
+    } else {
+        return `${readableNumbersFromZeroToNineteen(String(number).slice(0, 1))} hundred ${toReadableLessOneHundred(+String(number).slice(1))}`
+    }
+};
+
+function readableNumbersFromZeroToNineteen(number) {
+    let firstTwentyNumbers = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    return firstTwentyNumbers[number]
+}
+
+function redableTensNumbers(number) {
+    let tenNumbers = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+    return tenNumbers[(number / 10) - 2];
+}
+
+function toReadableLessOneHundred(number) {
+    let result = '';
+    if (number < 20) {
+        result += readableNumbersFromZeroToNineteen(number);
+        return result;
+    } else if (number % 10 === 0) {
+        result += redableTensNumbers(number);
+        return result;
+    } else {
+        return result = `${redableTensNumbers(String(number).slice(0,1) * 10)} ${readableNumbersFromZeroToNineteen(String(number).slice(1))}`
+    }
 }
